1177cb099807e20d12c02591e35050a2b673811c
itplamen <itplamen@gmail.com>
2025-10-03 00:13:46 +0300

    Enhanced ticket wager microservice

--- LotteryGame/WagerService/Api/WagerService.Api/Services/TicketService.cs
+++ LotteryGame/WagerService/Api/WagerService.Api/Services/TicketService.cs
@@ -1,6 +1,8 @@
 namespace WagerService.Api.Services
 {
     using AutoMapper;
+   
+    using Google.Protobuf.WellKnownTypes;
     
     using Grpc.Core;
     
@@ -11,17 +13,28 @@ namespace WagerService.Api.Services
     public class TicketService : Tickets.TicketsBase
     {
         private readonly IMapper mapper;
-        private ITicketOperations ticketOperations;
+        private readonly IConfiguration configuration;
+        private readonly ITicketOperations ticketOperations;
 
-        public TicketService(IMapper mapper, ITicketOperations ticketOperations)
+        public TicketService(IMapper mapper, IConfiguration configuration, ITicketOperations ticketOperations)
         {
             this.mapper = mapper;
+            this.configuration = configuration;
             this.ticketOperations = ticketOperations;
+        }
+
+        public override Task<PriceResponse> GetPrice(Empty request, ServerCallContext context)
+        {
+            long ticketPriceInCents = long.Parse(configuration["TicketPriceCents"]);
+
+            return Task.FromResult(new PriceResponse() { Price = ticketPriceInCents });
         }
 
         public override async Task<TicketResponse> Create(TicketCreateRequest request, ServerCallContext context)
         {
             TicketCreateRequestDto requestDto = mapper.Map<TicketCreateRequestDto>(request);
+            requestDto.Amount = long.Parse(configuration["TicketPriceCents"]);
+
             ResponseDto<TicketDto> responseDto = await ticketOperations.Create(requestDto);
             TicketResponse response = mapper.Map<TicketResponse>(responseDto);
 
