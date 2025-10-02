1db46015be4b193997417b1840175c4a6d9f3b79
itplamen <itplamen@gmail.com>
2025-10-02 20:23:12 +0300

    Created wallet microservice and protos

---
+++ LotteryGame/WalletService/Api/WalletService.Api/Program.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using WalletService.Api.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<FundsService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
---
+++ LotteryGame/WalletService/Api/WalletService.Api/Services/FundsService.cs
@@ -0,0 +1,32 @@
+﻿namespace WalletService.Api.Services
+{
+    using System.Threading.Tasks;
+
+    using AutoMapper;
+    
+    using Grpc.Core;
+    
+    using WalletService.Api.Models.Protos.Funds;
+    using WalletService.Core.Contracts;
+    using WalletService.Core.Models;
+
+    public class FundsService : Funds.FundsBase
+    {
+        private readonly IMapper mapper;
+        private readonly IFundsOperations fundsOperations;
+
+        public FundsService(IMapper mapper, IFundsOperations fundsOperations)
+        {
+            this.mapper = mapper;
+            this.fundsOperations = fundsOperations;
+        }
+
+        public override async Task<EnoughFundsResponse> HasEnoughFunds(EnoughFundsRequest request, ServerCallContext context)
+        {
+            ResponseDto responseDto = await fundsOperations.HasEnoughFunds(request.PlayerId, request.CostAmount);
+            EnoughFundsResponse response = mapper.Map<EnoughFundsResponse>(responseDto);
+
+            return response;
+        }
+    }
+}
