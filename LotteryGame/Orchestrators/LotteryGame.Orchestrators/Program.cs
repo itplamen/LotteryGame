8c4f7dd49256c572dcc420857b9e733519a89da0
itplamen <itplamen@gmail.com>
2025-10-03 00:15:10 +0300

    Added configurations

--- LotteryGame/Orchestrators/LotteryGame.Orchestrators/Program.cs
+++ LotteryGame/Orchestrators/LotteryGame.Orchestrators/Program.cs
@@ -1,6 +1,7 @@
 using LotteryGame.Orchestrators.Services;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
+using WagerService.Api.Models.Protos.Tickets;
+using WalletService.Api.Models.Protos.Funds;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +11,16 @@ builder.Services.AddGrpc();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<FundsOrchestratorService>();
+app.MapGrpcService<TicketPurchaseOrchestrator>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+builder.Services.AddGrpcClient<Funds.FundsClient>(x =>
+{
+    x.Address = new Uri("https://wagerservice:5001");
+});
+builder.Services.AddGrpcClient<Tickets.TicketsClient>(x =>
+{
+    x.Address = new Uri("https://wagerservice:5002");
+});
 
 app.Run();
