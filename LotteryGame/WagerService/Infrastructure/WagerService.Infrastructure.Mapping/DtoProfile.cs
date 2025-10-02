15a5dbb84390c1db51cbb3a273447d230df10499
itplamen <itplamen@gmail.com>
2025-10-02 09:07:12 +0300

    Created infrastructure mapping

---
+++ LotteryGame/WagerService/Infrastructure/WagerService.Infrastructure.Mapping/DtoProfile.cs
@@ -0,0 +1,18 @@
+﻿namespace WagerService.Infrastructure.Mapping
+{
+    using AutoMapper;
+
+    using WagerService.Core.Models;
+    using WagerService.Data.Models;
+
+    public class DtoProfile : Profile
+    {
+        public DtoProfile()
+        {
+            CreateMap<Ticket, TicketDto>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.TicketNumber, opt => opt.MapFrom(src => src.TicketNumber))
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status));
+        }
+    }
+}
