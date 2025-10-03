7eb0b26f67696a67aac7d602dbeba51bc2c284e9
itplamen <itplamen@gmail.com>
2025-10-03 08:58:37 +0300

    Create dto models for draw microservice

---
+++ LotteryGame/DrawService/Core/DrawService.Core.Models/BaseDto.cs
@@ -0,0 +1,14 @@
+﻿namespace DrawService.Core.Models
+{
+    public class BaseDto
+    {
+        public BaseDto() { }
+
+        public BaseDto(string id)
+        {
+            Id = id;
+        }
+
+        public string Id { get; set; }
+    }
+}
---
+++ LotteryGame/DrawService/Core/DrawService.Core.Models/DrawDto.cs
@@ -0,0 +1,11 @@
+﻿namespace DrawService.Core.Models
+{
+    using DrawService.Data.Models;
+
+    public class DrawDto : BaseDto
+    {
+        public DateTime DrawDate { get; set; }
+
+        public DrawStatus Status { get; set; }
+    }
+}
