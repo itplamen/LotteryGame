35473367453387951e089e85a3c500bfa7aa7bac
itplamen <itplamen@gmail.com>
2025-10-03 21:31:53 +0300

    Created prize dto

---
+++ LotteryGame/DrawService/Core/DrawService.Core.Models/PrizeDto.cs
@@ -0,0 +1,15 @@
+﻿namespace DrawService.Core.Models
+{
+    using DrawService.Data.Models;
+
+    public class PrizeDto : BaseDto
+    {
+        public string TicketId { get; set; }
+
+        public long Amount { get; set; }
+
+        public PrizeTier Tier { get; set; }
+
+        public string DrawId { get; set; }
+    }
+}
