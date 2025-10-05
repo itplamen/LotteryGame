94d89ff064c5394f704a1287194e23a93e50dfc9
itplamen <itplamen@gmail.com>
2025-10-05 17:02:14 +0300

    Created IoC packages for draw microservice

---
+++ LotteryGame/DrawService/Api/DrawService.Api/IoC/CorePackage.cs
@@ -0,0 +1,28 @@
+﻿namespace DrawService.Api.IoC
+{
+    using AutoMapper;
+    
+    using DrawService.Api.Mapping;
+    using DrawService.Core.Contracts;
+    using DrawService.Core.Operations;
+    using DrawService.Core.PrizeDeterminations;
+    using LotteryGame.Common.Models.IoC;
+
+    public sealed class CorePackage : IPackage
+    {
+        public void RegisterServices(IServiceCollection services)
+        {
+            services.AddAutoMapper(x => x.AddProfiles(new List<Profile>() { new DataProfile(), new ProtosProfile() }));
+            //services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.AddTransient<IPrizeStrategy, GrandPrizeStrategy>();
+            services.AddTransient<IPrizeStrategy, SecondTierPrizeStrategy>();
+            services.AddTransient<IPrizeStrategy, ThirdTierPrizeStrategy>();
+
+            services.AddTransient<IPrizeDeterminationStrategy, PrizeDeterminationStrategy>();
+
+            services.AddScoped<IDrawOperations, DrawOperations>();
+            services.AddScoped<IPrizeOperations, PrizeOperations>();
+        }
+    }
+}
---
+++ LotteryGame/DrawService/Api/DrawService.Api/IoC/DataPackage.cs
@@ -0,0 +1,33 @@
+﻿namespace DrawService.Api.IoC
+{
+    using CouchDB.Driver.DependencyInjection;
+    using DrawService.Data;
+    using DrawService.Data.Contracts;
+    using LotteryGame.Common.Models.IoC;
+
+    public sealed class DataPackage : IPackage
+    {
+        private readonly IConfiguration configuration;
+
+        public DataPackage(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public void RegisterServices(IServiceCollection services)
+        {
+            services.AddCouchContext<DrawServiceDbContext>(builder =>
+            {
+                builder
+                    .UseEndpoint(configuration["CouchDB:Endpoint"])
+                    .EnsureDatabaseExists()
+                    .UseBasicAuthentication(
+                        username: configuration["CouchDB:Username"],
+                        password: configuration["CouchDB:Password"]
+                    );
+            });
+
+            services.AddScoped(typeof(IRepository<>), typeof(DbRepository<>));
+        }
+    }
+}
