086d116ebbcae46e2a6f35ac5d3fee96801eec40
itplamen <itplamen@gmail.com>
2025-10-05 17:01:59 +0300

    Created mapping profiles for draw miroservice

---
+++ LotteryGame/DrawService/Api/DrawService.Api/Mapping/DataProfile.cs
@@ -0,0 +1,25 @@
+﻿namespace DrawService.Api.Mapping
+{
+    using AutoMapper;
+    
+    using DrawService.Core.Models;
+    using DrawService.Data.Models;
+
+    public class DataProfile : Profile
+    {
+        public DataProfile()
+        {
+            CreateMap<Prize, PrizeDto>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.TicketId, opt => opt.MapFrom(src => src.TicketId))
+                .ForMember(dest => dest.Tier, opt => opt.MapFrom(src => src.Tier))
+                .ForMember(dest => dest.Amount, opt => opt.MapFrom(src => src.Amount))
+                .ForMember(dest => dest.DrawId, opt => opt.MapFrom(src => src.DrawId));
+
+            CreateMap<Draw, DrawDto>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status))
+                .ForMember(dest => dest.TicketPriceInCents, opt => opt.MapFrom(src => src.TicketPriceInCents));
+        }
+    }
+}
---
+++ LotteryGame/DrawService/Api/DrawService.Api/Mapping/ProtosProfile.cs
@@ -0,0 +1,29 @@
+﻿namespace DrawService.Api.Mapping
+{
+    using AutoMapper;
+    
+    using Google.Protobuf.WellKnownTypes;
+    
+    using DrawService.Api.Models.Protos.Draws;
+    using DrawService.Core.Models;
+    using LotteryGame.Common.Models.Dto;
+
+    public class ProtosProfile : Profile
+    {
+        public ProtosProfile()
+        {
+            CreateMap<ResponseDto<DrawDto>, FetchDrawResponse>()
+                .ForMember(dest => dest.ErrorMsg, opt => opt.MapFrom(src => src.ErrorMsg))
+                .ForMember(dest => dest.Success, opt => opt.MapFrom(src => src.IsSuccess))
+                .ForMember(dest => dest.DrawId, opt => opt.MapFrom(src => src.Data.Id))
+                .ForMember(dest => dest.TicketPriceInCents, opt => opt.MapFrom(src => src.Data.TicketPriceInCents))
+                .ForMember(dest => dest.MinTicketsPerPlayer, opt => opt.MapFrom(src => src.Data.MinTicketsPerPlayer))
+                .ForMember(dest => dest.MaxTicketsPerPlayer, opt => opt.MapFrom(src => src.Data.MaxTicketsPerPlayer))
+                .ForMember(dest => dest.DrawDate, opt => opt.MapFrom(src => Timestamp.FromDateTime(src.Data.DrawDate.ToUniversalTime())));
+
+            CreateMap<ResponseDto, DrawResponse>()
+                .ForMember(dest => dest.ErrorMsg, opt => opt.MapFrom(src => src.ErrorMsg))
+                .ForMember(dest => dest.Success, opt => opt.MapFrom(src => src.IsSuccess));
+        }
+    }
+}
