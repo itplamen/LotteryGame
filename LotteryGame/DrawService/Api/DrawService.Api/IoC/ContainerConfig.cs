be0e7f5008e722d3020ca9327983cc83f416cd9d
itplamen <itplamen@gmail.com>
2025-10-05 17:02:28 +0300

    Created IoC config

---
+++ LotteryGame/DrawService/Api/DrawService.Api/IoC/ContainerConfig.cs
@@ -0,0 +1,26 @@
+﻿namespace DrawService.Api.IoC
+{
+    using LotteryGame.Common.Models.IoC;
+
+    public static class ContainerConfig
+    {
+        public static void AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            IPackage[] packages =
+            {
+                new CorePackage(),
+                new DataPackage(configuration)
+            };
+
+            RegisterServices(services, packages);
+        }
+
+        private static void RegisterServices(IServiceCollection services, IPackage[] packages)
+        {
+            foreach (var package in packages)
+            {
+                package.RegisterServices(services);
+            }
+        }
+    }
+}
