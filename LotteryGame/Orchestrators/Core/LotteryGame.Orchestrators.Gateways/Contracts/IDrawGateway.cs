5c8b08cfa32dafe2f5a0e24eb8e99b60aea14902
itplamen <itplamen@gmail.com>
2025-10-06 21:44:46 +0300

    Created draw gateway

---
+++ LotteryGame/Orchestrators/Core/LotteryGame.Orchestrators.Gateways/Contracts/IDrawGateway.cs
@@ -0,0 +1,9 @@
+﻿namespace LotteryGame.Orchestrators.Gateways.Contracts
+{
+    using DrawService.Api.Models.Protos.Draws;
+
+    public interface IDrawGateway
+    {
+        Task<FetchDrawResponse> GetOpenDraw(int playerId);
+    }
+}
---
+++ LotteryGame/Orchestrators/Core/LotteryGame.Orchestrators.Gateways/DrawGateway.cs
@@ -0,0 +1,28 @@
+﻿namespace LotteryGame.Orchestrators.Gateways
+{
+    using Microsoft.Extensions.Configuration;
+
+    using DrawService.Api.Models.Protos.Draws;
+    using LotteryGame.Orchestrators.Gateways.Contracts;
+    
+    public class DrawGateway : BaseGateway, IDrawGateway
+    {
+        private readonly Draws.DrawsClient drawClient;
+
+        public DrawGateway(Draws.DrawsClient drawClient, IConfiguration configuration)
+            : base(configuration)
+        {
+            this.drawClient = drawClient;
+        }
+
+        public async Task<FetchDrawResponse> GetOpenDraw(int playerId)
+        {
+            FetchDrawRequest fetchDrawRequest = new FetchDrawRequest()
+            {
+                PlayerId = playerId
+            };
+
+            return await Execute(async () => await drawClient.FetchDrawAsync(fetchDrawRequest));
+        }
+    }
+}
