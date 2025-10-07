6256590cf6cee554ef8dd13e821adec64d1ae333
itplamen <itplamen@gmail.com>
2025-10-07 15:05:08 +0300

    Created logging orchestrator

--- LotteryGame/Orchestrators/Api/LotteryGame.Orchestrators.Api/IoC/OrchestratorsPackage.cs
+++ LotteryGame/Orchestrators/Api/LotteryGame.Orchestrators.Api/IoC/OrchestratorsPackage.cs
@@ -1,8 +1,10 @@
 ﻿namespace LotteryGame.Orchestrators.Api.IoC
 {
     using LotteryGame.Common.Models.IoC;
+    using LotteryGame.Orchestrators.Cache.Contracts;
     using LotteryGame.Orchestrators.Contracts;
     using LotteryGame.Orchestrators.Models.AvailableDraw;
+    using LotteryGame.Orchestrators.Models.Base;
     using LotteryGame.Orchestrators.Models.DrawParticipation;
     using LotteryGame.Orchestrators.Models.PurchaseTickets;
     using LotteryGame.Orchestrators.Models.ReserveFunds;
@@ -11,10 +13,29 @@
     {
         public void RegisterServices(IServiceCollection services)
         {
-            services.AddTransient<IOrchestrator<AvailableDrawRequest, AvailableDrawResponse>, AvailableDrawOrchestrator>();
-            services.AddTransient<IOrchestrator<DrawParticipationRequest, DrawParticipationResponse>, DrawParticipationOrchestrator>();
-            services.AddTransient<IOrchestrator<PurchaseTicketsRequest, PurchaseTicketsResponse>, PurchaseTicketsOrchestrator>();
-            services.AddTransient<IOrchestrator<ReserveFundsRequest, ReserveFundsResponse>, ReserveFunsOrchestrator>();
+            services.AddTransient<AvailableDrawOrchestrator>();
+            services.AddTransient<DrawParticipationOrchestrator>();
+            services.AddTransient<PurchaseTicketsOrchestrator>();
+            services.AddTransient<ReserveFunsOrchestrator>();
+
+            RegisterDecorator<AvailableDrawRequest, AvailableDrawResponse, AvailableDrawOrchestrator>(services);
+            RegisterDecorator<DrawParticipationRequest, DrawParticipationResponse, DrawParticipationOrchestrator>(services);
+            RegisterDecorator<PurchaseTicketsRequest, PurchaseTicketsResponse, PurchaseTicketsOrchestrator>(services);
+            RegisterDecorator<ReserveFundsRequest, ReserveFundsResponse, ReserveFunsOrchestrator>(services);
+        }
+
+        private void RegisterDecorator<TRequest, TResponse, TOrchestrator>(IServiceCollection services)
+            where TRequest : class
+            where TResponse : class
+            where TOrchestrator : IOrchestrator<TRequest, TResponse>
+        {
+            services.AddTransient<IOrchestrator<TRequest, TResponse>>(x =>
+            {
+                var inner = x.GetRequiredService<TOrchestrator>();
+                var cache = x.GetRequiredService<ICacheService<IDictionary<TRequest, OrchestratorResponse<TResponse>>>>();
+
+                return new LoggingOrchestratorDecorator<TRequest, TResponse>(inner, cache);
+            });
         }
     }
 }
---
+++ LotteryGame/Orchestrators/Core/LotteryGame.Orchestrators/LoggingOrchestratorDecorator.cs
@@ -0,0 +1,32 @@
+﻿namespace LotteryGame.Orchestrators
+{
+    using LotteryGame.Orchestrators.Cache.Contracts;
+    using LotteryGame.Orchestrators.Contracts;
+    using LotteryGame.Orchestrators.Models.Base;
+
+    public class LoggingOrchestratorDecorator<TRequest, TResponse> : IOrchestrator<TRequest, TResponse>
+    {
+        private readonly IOrchestrator<TRequest, TResponse> decoratee;
+        private readonly ICacheService<IDictionary<TRequest, OrchestratorResponse<TResponse>>> cacheService;
+
+        public LoggingOrchestratorDecorator(
+            IOrchestrator<TRequest, TResponse> decoratee, 
+            ICacheService<IDictionary<TRequest, OrchestratorResponse<TResponse>>> cacheService)
+        {
+            this.decoratee = decoratee;
+            this.cacheService = cacheService;
+        }
+
+        public async Task<OrchestratorResponse<TResponse>> Orchestrate(OrchestratorRequest<TRequest> request)
+        {
+            IDictionary<TRequest, OrchestratorResponse<TResponse>> result = await cacheService.GetOrCreateAsync(request.CorrelationId, async () =>
+            {
+                OrchestratorResponse<TResponse> response = await decoratee.Orchestrate(request);
+
+                return new Dictionary<TRequest, OrchestratorResponse<TResponse>>() { { request.Payload, response } };
+            });
+
+            return result.Values.FirstOrDefault();
+        }
+    }
+}
