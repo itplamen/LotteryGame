f1f3993b5d4d16738c50f3f6bd26c75703b92e6a
itplamen <itplamen@gmail.com>
2025-10-09 00:00:49 +0300

    Create common validation policies

---
+++ LotteryGame/Common/LotteryGame.Common.Utils/Validation/IOperationPolicy.cs
@@ -0,0 +1,9 @@
+﻿namespace LotteryGame.Common.Utils.Validation
+{
+    using LotteryGame.Common.Models.Dto;
+
+    public interface IOperationPolicy<TContext>
+    {
+        Task<ResponseDto> ExecuteAsync(TContext context);
+    }
+}
---
+++ LotteryGame/Common/LotteryGame.Common.Utils/Validation/OperationPipeline.cs
@@ -0,0 +1,28 @@
+﻿namespace LotteryGame.Common.Utils.Validation
+{
+    using LotteryGame.Common.Models.Dto;
+
+    public class OperationPipeline<TContext>
+    {
+        private readonly IEnumerable<IOperationPolicy<TContext>> policies;
+
+        public OperationPipeline(IEnumerable<IOperationPolicy<TContext>> policies)
+        {
+            this.policies = policies;
+        }
+
+        public async Task<ResponseDto> ExecuteAsync(TContext context)
+        {
+            foreach (var policy in policies)
+            {
+                var error = await policy.ExecuteAsync(context);
+                if (!error.IsSuccess)
+                {
+                    return error;
+                }
+            }
+
+            return new ResponseDto();
+        }
+    }
+}
