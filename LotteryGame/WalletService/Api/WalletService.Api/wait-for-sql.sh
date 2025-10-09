059a8682c830b3cd9a3a76c6286061391e89f816
itplamen <itplamen@gmail.com>
2025-10-10 01:43:34 +0300

    Added PowerShell script waiting for sql server

---
+++ LotteryGame/WalletService/Api/WalletService.Api/wait-for-sql.sh
@@ -0,0 +1,16 @@
+﻿#!/usr/bin/env bash
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+echo "Checking SQL Server at $host..."
+
+until /opt/mssql-tools/bin/sqlcmd -S "$host" -U sa -P "YourStrong!Passw0rd" -Q "SELECT 1" >/dev/null 2>&1; do
+  echo "Waiting for SQL Server at $host..."
+  sleep 5
+done
+
+echo
+exec $cmd
