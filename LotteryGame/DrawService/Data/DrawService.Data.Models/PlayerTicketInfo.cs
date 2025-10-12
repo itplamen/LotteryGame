df15b5a77a7f0233b07b1fcee7d7306afcb347ee
itplamen <itplamen@gmail.com>
2025-10-12 04:02:04 +0300

    Create additional model

---
+++ LotteryGame/DrawService/Data/DrawService.Data.Models/PlayerTicketInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace DrawService.Data.Models
+{
+    public class PlayerTicketInfo
+    {
+        public int PlayerId { get; set; }
+
+        public List<string> Tickets { get; set; } = new List<string>();
+    }
+}
